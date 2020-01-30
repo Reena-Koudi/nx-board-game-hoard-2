3974ae5c50a6001e983d0c87490086b6fb40818e
Reena Koudi <reena.koudi@holition.com>
2020-01-30 11:10:18 +0000

    split the review site into separate libraries

---
+++ libs/review/data-access-games/src/lib/review-data-access-games.tsx
@@ -0,0 +1,16 @@
+const [games, setGames] = useState<Game[]>([]);
+
+import React, { useState, useEffect } from 'react';
+import { Game } from '@bghoard/api-interfaces';
+
+export function useGames() {
+  const [games, setGame] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('/api/game')
+      .then(r => r.json())
+      .then(setGame);
+  }, []);
+
+  return games;
+}
